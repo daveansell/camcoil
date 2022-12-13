7778e98a8780f82b19f5ffde0389ff439999309e
Dave Ansell <dave@sciansell.co.uk>
2022-12-13 13:06:27 +0000

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import coil
 
 c = coil.coil(wireWidth=0.6)
 
-c.add_coil(500, 1, 0.0, 30, freeStanding=True, wireWidth=0.63, mode='bobbin')
+c.add_coil(100, 1, 0.0, 4.0, freeStanding=True, wireWidth=0.63, mode='bobbin')
 #c.toAngle( 0.5, 1)
 #c.toAngle( 0, 1)
 c.renderFile('test')
